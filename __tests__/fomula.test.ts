c4eef37599fba3e58798dd83c08a944450b70854
liaoxuezhi <2betop.cn@gmail.com>
2021-11-27 17:21:25 +0800

    添加用例

--- __tests__/fomula.test.ts
+++ __tests__/fomula.test.ts
@@ -44,6 +44,71 @@ test('formula:expression', () => {
   expect(evalFormual('3 >> 1')).toBe(1);
   expect(evalFormual('3 << 1')).toBe(6);
   expect(evalFormual('10 ** 3')).toBe(1000);
+
+  expect(evalFormual('10 ? 3 : 2')).toBe(3);
+  expect(evalFormual('0 ? 3 : 2')).toBe(2);
+});
+
+test('formula:if', () => {
+  expect(evalFormual('IF(true, 2, 3)')).toBe(2);
+  expect(evalFormual('IF(false, 2, 3)')).toBe(3);
+  expect(evalFormual('IF(false, 2, IF(true, 3, 4))')).toBe(3);
+});
+
+test('formula:and', () => {
+  expect(!!evalFormual('AND(0, 1)')).toBe(false);
+  expect(!!evalFormual('AND(1, 1)')).toBe(true);
+  expect(!!evalFormual('AND(1, 1, 1, 0)')).toBe(false);
+});
+
+test('formula:or', () => {
+  expect(!!evalFormual('OR(0, 1)')).toBe(true);
+  expect(!!evalFormual('OR(1, 1)')).toBe(true);
+  expect(!!evalFormual('OR(1, 1, 1, 0)')).toBe(true);
+  expect(!!evalFormual('OR(0, 0, 0, 0)')).toBe(false);
+});
+
+test('formula:xor', () => {
+  expect(evalFormual('XOR(0, 1)')).toBe(false);
+  expect(evalFormual('XOR(1, 0)')).toBe(false);
+  expect(evalFormual('XOR(1, 1)')).toBe(true);
+  expect(evalFormual('XOR(0, 0)')).toBe(true);
+});
+
+test('formula:ifs', () => {
+  expect(!!evalFormual('IFS(0, 1, 2)')).toBe(true);
+  expect(!!evalFormual('IFS(0, 1, 2, 2, 3)')).toBe(true);
+  expect(!!evalFormual('IFS(0, 1, 0, 2, 0)')).toBe(false);
+});
+test('formula:math', () => {
+  expect(evalFormual('ABS(1)')).toBe(1);
+  expect(evalFormual('ABS(-1)')).toBe(1);
+  expect(evalFormual('ABS(0)')).toBe(0);
+
+  expect(evalFormual('MAX(1, -1, 2, 3, 5, -9)')).toBe(5);
+  expect(evalFormual('MIN(1, -1, 2, 3, 5, -9)')).toBe(-9);
+
+  expect(evalFormual('MOD(3, 2)')).toBe(1);
+
+  expect(evalFormual('PI()')).toBe(Math.PI);
+
+  expect(evalFormual('ROUND(3.5)')).toBe(4);
+  expect(evalFormual('ROUND(3.4)')).toBe(3);
+
+  expect(evalFormual('ROUND(3.456789, 2)')).toBe(3.46);
+  expect(evalFormual('CEIL(3.456789)')).toBe(4);
+  expect(evalFormual('FLOOR(3.456789)')).toBe(3);
+
+  expect(evalFormual('SQRT(4)')).toBe(2);
+  expect(evalFormual('AVG(4, 6, 10, 10, 10)')).toBe(8);
+
+  expect(evalFormual('UPPERMONEY(7682.01)')).toBe('柒仟陆佰捌拾贰元壹分');
+  expect(evalFormual('UPPERMONEY(7682)')).toBe('柒仟陆佰捌拾贰元整');
+
+  // 非数字类型转换是否正常？
+  expect(evalFormual('"3" + "3"')).toBe(6);
+  expect(evalFormual('"3" - "3"')).toBe(0);
+  expect(evalFormual('AVG(4, "6", "10", 10, 10)')).toBe(8);
 });
 
 test('formula:date', () => {
--- src/evalutor.ts
+++ src/evalutor.ts
@@ -447,15 +447,22 @@ export class Evaluator {
   // 数学函数
 
   fnABS(a: number) {
+    a = this.formatNumber(a);
     return Math.abs(a);
   }
 
-  fnMax(...args: Array<any>) {
-    return Math.max.apply(Math, args);
+  fnMAX(...args: Array<any>) {
+    return Math.max.apply(
+      Math,
+      args.map(item => this.formatNumber(item))
+    );
   }
 
-  fnMin(...args: Array<number>) {
-    return Math.min.apply(Math, args);
+  fnMIN(...args: Array<number>) {
+    return Math.min.apply(
+      Math,
+      args.map(item => this.formatNumber(item))
+    );
   }
 
   fnSUM(...args: Array<number>) {
@@ -463,11 +470,11 @@ export class Evaluator {
   }
 
   fnINT(n: number) {
-    return Math.floor(n);
+    return Math.floor(this.formatNumber(n));
   }
 
   fnMOD(a: number, b: number) {
-    return a % b;
+    return this.formatNumber(a) % this.formatNumber(b);
   }
 
   fnPI() {
@@ -475,6 +482,8 @@ export class Evaluator {
   }
 
   fnROUND(a: number, b: number) {
+    a = this.formatNumber(a);
+    b = this.formatNumber(b);
     const bResult = Math.round(b);
 
     if (bResult) {
@@ -485,16 +494,78 @@ export class Evaluator {
     return Math.round(a);
   }
 
+  fnFLOOR(a: number, b: number) {
+    a = this.formatNumber(a);
+    b = this.formatNumber(b);
+    const bResult = Math.round(b);
+
+    if (bResult) {
+      const c = Math.pow(10, bResult);
+      return Math.floor(a * c) / c;
+    }
+
+    return Math.floor(a);
+  }
+
+  fnCEIL(a: number, b: number) {
+    a = this.formatNumber(a);
+    b = this.formatNumber(b);
+    const bResult = Math.round(b);
+
+    if (bResult) {
+      const c = Math.pow(10, bResult);
+      return Math.ceil(a * c) / c;
+    }
+
+    return Math.ceil(a);
+  }
+
   fnSQRT(n: number) {
-    return Math.sqrt(n);
+    return Math.sqrt(this.formatNumber(n));
   }
 
   fnAVG(...args: Array<any>) {
-    return this.fnSUM.apply(this, args) / args.length;
+    return (
+      this.fnSUM.apply(
+        this,
+        args.map(item => this.formatNumber(item))
+      ) / args.length
+    );
   }
 
-  fnUPPERMONEY() {
-    return 'todo';
+  fnUPPERMONEY(n: number) {
+    n = this.formatNumber(n);
+    const fraction = ['角', '分'];
+    const digit = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'];
+    const unit = [
+      ['元', '万', '亿'],
+      ['', '拾', '佰', '仟']
+    ];
+    const head = n < 0 ? '欠' : '';
+    n = Math.abs(n);
+    let s = '';
+    for (let i = 0; i < fraction.length; i++) {
+      s += (
+        digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + fraction[i]
+      ).replace(/零./, '');
+    }
+    s = s || '整';
+    n = Math.floor(n);
+    for (let i = 0; i < unit[0].length && n > 0; i++) {
+      let p = '';
+      for (let j = 0; j < unit[1].length && n > 0; j++) {
+        p = digit[n % 10] + unit[1][j] + p;
+        n = Math.floor(n / 10);
+      }
+      s = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][i] + s;
+    }
+    return (
+      head +
+      s
+        .replace(/(零.)*零元/, '元')
+        .replace(/(零.)+/g, '零')
+        .replace(/^整$/, '零元整')
+    );
   }
 
   fnRAND() {
