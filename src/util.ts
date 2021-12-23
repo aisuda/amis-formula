daa54654769bcd5cffe85e5ecf5d6ee3f3e5015d
2betop <2betop.cn@gmail.com>
2021-12-23 15:57:49 +0800

    cd '/Users/liaoxuezhi/project/amis-formula'

--- src/util.ts
+++ src/util.ts
@@ -398,8 +398,14 @@ export function resolveVariable(path?: string, data: any = {}): any {
     return data;
   } else if (!path || typeof path !== 'string') {
     return undefined;
+  } else if (!~path.indexOf(':')) {
+    // 简单用法直接用 getVariable
+    return getVariable(data, path);
   }
 
+  // window:xxx  ls:xxx.xxx
+  // 带 namespace 的用公式
+  // 主要是用公式会严格点，不能出现奇怪的变量名
   try {
     return new Evaluator(data).evalute(
       parse(path, {
